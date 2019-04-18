2628bcb1da2473635f356053d6c3e69c5eebad82
hatobus <s1240056@gmail.com>
2019-04-18 16:48:40 +0900

    debug devide operand changed

--- python/zwed.py
+++ python/zwed.py
@@ -8,14 +8,20 @@ class ZWED():
     def __init__(self):
         pass
 
-    # Exchange base N --> 10
+    # Exchange base 10 --> N
     def B10toN(self, num, base):
-        isdev = int(num / base)
-        if (isdev):
-            return self.B10toN(isdev, base) + str(num%base)
-        return str(num % base)
+        res = []
+        while num:
+            amari =str(num % base)
+            res.insert(0, amari)
+            num = num // base
+            if num == 0:
+                break
+
+        s = "".join(res)
+        return s
     
-    # Excange base 10 --> N
+    # Excange base N --> 10
     def BNto10(self, num, base):
         out = 0
     
@@ -25,14 +31,17 @@ class ZWED():
         return out
     
     def Encode(self, string):
-        string = string.replace("\n", "")
+        #string = string.replace("\n", "")
         
         bytstr = binascii.hexlify(string.encode("utf-8"))
         hexstr = bytstr.decode("utf-8")
+
+        # base 16 --> 10
         dec = int(hexstr, 16)
 
         quatstr = self.B10toN(dec, 4)
 
+        # Replace zero width spaces
         for d, s in ZWSP.items():
             quatstr = quatstr.replace(d, s)
         
@@ -42,20 +51,17 @@ class ZWED():
         for d, s in ZWSP.items():
             zwstr = zwstr.replace(s, d) 
     
+        # base 4 --> 10
         dec = self.BNto10(zwstr, 4)
-        print(dec)
-        hexstr = hex(dec)[2:]
-        print(hexstr)
 
-        print(binascii.unhexlify(hexstr.encode("utf-8")))
+        hexstr = hex(dec)[2:]
 
         return str(binascii.unhexlify(hexstr), 'utf-8')
 
 if __name__ == "__main__":
     ZW = ZWED()
-    #zwstr = ZW.Encode("あいうえお")
-    zwstr = ZW.Encode("abcdefgh")
+    zwstr = ZW.Encode("こんにちは")
+    #zwstr = ZW.Encode("abcdefgh")
     print(zwstr)
     decoded = ZW.Decode(zwstr)
     print(decoded)
-    #print(decoded.decode('utf-8'))
