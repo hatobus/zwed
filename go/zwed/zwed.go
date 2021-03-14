a4d9c2e7418d61d79d21fd14a842dc0b2951ace1
hatobus <s1240056@gmail.com>
2021-03-14 19:35:01 +0900

    全ての文字をdecodeするように

--- go/zwed/zwed.go
+++ go/zwed/zwed.go
@@ -14,7 +14,6 @@ var (
 		"2": "\u200d",
 		"3": "\u200e",
 	}
-	//delim = "\u034f"
 )
 
 func SetZWSpace(m map[string]string) {
@@ -54,52 +53,83 @@ func Decode(dst string) (string, error) {
 		return "", err
 	}
 
-	bpStart := 0
 	bpEnd := 4
 
-	// 1文字目のbit幅を取得するためunicodeのビット幅の情報を持つ部分を取得
-	// 最初の4bitに情報が入ってきており、ここでは4進数に変換されているので4桁を取ってくれば良い
-	bitWidthFour := dst2q[:bpEnd]
+	var byteSlice []byte
+	for ; len(dst2q) != 0 ; {
+		// 1文字目のbit幅を取得するためunicodeのビット幅の情報を持つ部分を取得
+		// 最初の4bitに情報が入ってきており、ここでは4進数に変換されているので4桁を取ってくれば良い
+		bitWidthFour := dst2q[:bpEnd]
 
+		firstCharBitWidth, err := getOctetsFromBitPattern(bitWidthFour)
+		if err != nil {
+			return "", err
+		}
+
+		bpEnd = firstCharBitWidth * 4
+		// 1文字目のbyte数がわかったのでbyte数*4の文だけsubstringを取得してくる
+		// (binaryのデータでは 1110 1001 なのが 3221 となるため)
+		charBytes := string([]rune(dst2q)[:bpEnd])
+		dst2q = string([]rune(dst2q)[bpEnd:])
+
+		// ここで得られた値を直接変えてしまうとだめなので１文字ずつ変換してあげる必要がある
+
+		bs, err := decodeCharFromQuadrant(charBytes)
+		if err != nil {
+			return "", err
+		}
+
+		byteSlice = append(byteSlice, bs...)
+	}
+
+	return string(byteSlice), nil
+}
+
+// 文字のBit幅を取得すためにUnicodeのビットパターンから有効ビット数を取得する
+// 最初の1byteデータを入力して頭についている1の数を数え上げる
+func getOctetsFromBitPattern(firstByte string) (int, error) {
 	// 4進数からUint8に変換する
-	bwUint, err := strconv.ParseUint(bitWidthFour, 4, 8)
+	bwUint, err := strconv.ParseUint(firstByte, 4, 64)
 	if err != nil {
-		return "", err
+		return -1, err
 	}
 
 	// 必要なbyte数を数える
-	var firstCharBitWidth int
+	var bitWidth int
 	for i := 8; i >= 0; i-- {
 		if refBitFromByte(int(bwUint), i-1) == 0 {
 			break
 		}
-		firstCharBitWidth++
+		bitWidth++
 	}
 
-	// 1文字目のbyte数がわかったのでbyte数*4の文だけsubstringを取得してくる
-	// (binaryのデータでは 1110 1001 なのが 3221 となるため)
-	firstCharBytes := string([]rune(dst2q)[bpStart:(firstCharBitWidth*4)])
+	return bitWidth, nil
+}
 
-	// ここで得られた値を直接変えてしまうとだめなので１文字ずつ変換してあげる必要がある
-	byteSlice := make([]byte, 0, 0)
+// 4進数で入ってきたデータをもとに文字を生成する
+// 4進数のデータを1つずつ2桁の2進数に変換し結合、それをbyteデータに変換して返す
+func decodeCharFromQuadrant(charBytes string) ([]byte, error) {
 	var s string
-	for i, fc := range strings.Split(firstCharBytes, "") {
+	var byteSlice []byte
+	for i, fc := range strings.Split(charBytes, "") {
 		s2i, err := strconv.Atoi(fc)
 		if err != nil {
-			return "", err
+			return byteSlice, err
 		}
 		s += fmt.Sprintf("%02b", s2i)
+
+		// 4進数のデータなので1byteは4文字に相当する
+		// 1byteが処理し終わったあとにbyte型に変換してbyteSliceに追加する
 		if (i+1) % 4 == 0 {
 			bi, err := strconv.ParseUint(s, 2, 32)
 			if err != nil {
-				return "", err
+				return byteSlice, err
 			}
 			byteSlice = append(byteSlice, byte(bi))
 			s = ""
 		}
 	}
-
-	return string(byteSlice), nil
+	return byteSlice, nil
 }
 
 func refBitFromByte(b int, i int) int {
@@ -137,23 +167,3 @@ func decimalToQuat(num int64) string {
 
 	return strings.Join(strarr, "")
 }
-
-func quatToDecimal(num string) int64 {
-	digits := strings.Split(num, "")
-
-	ex := 0
-
-	var out int64
-	out = 0
-	for i := len(digits) - 1; i >= 0; i-- {
-		n, err := strconv.ParseInt(digits[i], 10, 64)
-		if err != nil {
-			return 0
-		}
-		out += int64(math.Pow(4, float64(ex))) * n
-		ex++
-	}
-
-	return out
-}
-
